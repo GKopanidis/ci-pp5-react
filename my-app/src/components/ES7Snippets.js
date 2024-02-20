922efd4b2111ccee4251dfe161136dfd04b052fc
Georgios Kopanidis <kopanidis@me.com>
2024-02-20 12:14:05 +0000

    Add ES7Snippets component

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,51 @@
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>
+
+//       </div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+     
+//   }
+// }
+
+// console.log(object)
+
+// console.log('object', object)
