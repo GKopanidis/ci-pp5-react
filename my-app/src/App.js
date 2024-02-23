c5082cc7fe422d4568286bb2c4b45c705172269c
Georgios Kopanidis <kopanidis@me.com>
2024-02-23 13:49:26 +0000

    Add components to App.js and implement Loader component in Content.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
-import LifeCyclesCWU from "./components/LifeCyclesCWU";
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
-  return <div className="App">
-      <LifeCyclesCWU />
-    </div>;
+  return (
+    <div className={css.App}>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,29 +2,44 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import postsData from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 const { savedPosts } = postsData;
 
 export class Content extends Component {
-    // eslint-disable-next-line
     constructor(props) {
-        super(props);
+        super(props)
+        this.state = {
+            isLoaded: false
+        }
     }
 
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+    
     render() {
         return (
             <div className={css.Content}>
-
+                
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
                 </div>
 
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
                 </div>
             </div>
-        );
+        )
     }
 }
 
-export default Content;
+export default Content
