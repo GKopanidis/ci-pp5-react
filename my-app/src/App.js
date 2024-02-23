3d3a403555747a3997f67ef6e8da3e650f2d809d
Georgios Kopanidis <kopanidis@me.com>
2024-02-23 11:26:44 +0000

    Refactor App.js and add Book and RenderingLists components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,28 @@
-import css from "./App.module.css";
-// import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
+// import css from "./App.module.css";
+// // import NavBarSimple from "./components/NavBarSimple";
+// import Sidebar from "./components/Sidebar";
+// import NavBarForm from "./components/NavBarForm";
+
+// function App() {
+//   return (
+//     <div className={css.App}>
+//       {/* Add your components here */}
+//       <NavBarForm />
+//       <Sidebar />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
+
+import RenderingListsfrom from "./components/RenderingLists";
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarForm />
-      <Sidebar />
+  return <div className="App">
+    <RenderingListsfrom />
     </div>
-  );
 }
 
 export default App;
---
+++ my-app/src/components/Book.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function book(props) {
+    const book = props.book;
+    return (
+        <div>
+            <h5>{book.title}</h5>
+            <p>{book.author}</p>
+            <p>{book.pages}</p>
+        </div>
+    )
+
+}
+
+export default book
