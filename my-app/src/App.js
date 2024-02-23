ae234adf7bddc7ac990fed723964fe23eb59070e
Georgios Kopanidis <kopanidis@me.com>
2024-02-23 13:02:31 +0000

    Update life cycle methods in App.js and add new components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import LifeCyclesCDM from "./components/LifeCyclesCDM";
+import LifeCyclesCDU from "./components/LifeCyclesCDU";
 
 function App() {
   return <div className="App">
-      <LifeCyclesCDM />
+      <LifeCyclesCDU />
   </div>;
 }
 
