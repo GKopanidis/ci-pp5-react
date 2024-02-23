4b05ec5b8f690f987a3a80507d0ebadf9881de87
Georgios Kopanidis <kopanidis@me.com>
2024-02-23 14:14:07 +0000

    Refactor App.js and add ControlledForm component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,10 @@
-import css from "./App.module.css";
-// import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
+import "./App.css";
+import ControlledForm from "./components/ControlledForm";
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarForm />
-      <Sidebar />
-      <Content />
-    </div>
-  );
+  return <div className="App">
+      <ControlledForm />
+  </div>;
 }
 
 export default App;
---
+++ my-app/src/components/ControlledForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export class ControlledForm extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             name: ''
+        }
+    }
+    handleNameChange = (event) => {
+      this.setState({
+        name: event.target.value 
+      })
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor="id-name">Your Name:</label>
+                    <input
+                        value={this.state.name}
+                        onChange={this.handleNameChange}
+                        id="id-name"
+                        name="name"
+                        type="text"
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default ControlledForm
