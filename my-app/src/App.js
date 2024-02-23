bcb6758f57bb9cf0ac46ecbdb50806847ddf4a7e
Georgios Kopanidis <kopanidis@me.com>
2024-02-23 15:14:40 +0000

    Refactor App.js and Content.js components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
-import "./App.css";
-import { SearchBar } from "./components/SearchBar";
+import css from "./App.module.css";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
-  return <div className="App">
-      <SearchBar />
-  </div>;
+  return (
+    <div className={css.App}>
+      <NavBarForm />
+      <Sidebar />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
