90bdf2db7a1252aa1ddbe9e82899428d0cd05fa4
Georgios Kopanidis <kopanidis@me.com>
2024-02-24 11:05:04 +0000

    Install Axios, add dependencies and update files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,10 @@
-import css from "./App.module.css";
-// import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
-import ContentHooks from "./components/ContentHooks";
+import "./App.css";
+import HTTPRequests from "./components/HTTPRequests";
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarForm />
-      <Sidebar />
-      {/* <Content /> */}
-      <ContentHooks />
-    </div>
-  );
+  return <div className="App">
+      <HTTPRequests />
+  </div>;
 }
 
 export default App;
