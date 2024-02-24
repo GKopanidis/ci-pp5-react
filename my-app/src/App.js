04c5717f4cb4dc620fd9ac44e6e82d5efd2b9356
Georgios Kopanidis <kopanidis@me.com>
2024-02-24 09:30:21 +0000

    Refactor App.js and add UseStateWithArrays component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import ControlledFormHooks from "./components/ControlledFormHooks";
+import UseStateWithArrays from "./components/UseStateWithArrays";
 
 function App() {
   return <div className="App">
-      <ControlledFormHooks />
+      <UseStateWithArrays />
   </div>;
 }
 
