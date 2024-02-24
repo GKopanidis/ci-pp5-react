40d0a97eb3353633bae634b63705cecad6462fe8
Georgios Kopanidis <kopanidis@me.com>
2024-02-24 10:19:48 +0000

    Refactor App.js and UseEffectCounter.js

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("count 1 effect");
-    document.title = count;
-  });
-  const [count2, setCount2] = useState(0);
-  useEffect(() => {
-    console.log("count 2 effect");
-    document.title = count2;
-  });
-  return (
-    <div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        Increment Count ({count})
-      </button>
-      <button onClick={() => setCount2((count2) => count2 + 1)}>
-        Increment Count2 ({count2})
-      </button>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        console.log('count 1 effect');
+        document.title = count;
+    }, [count]);
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
+    return (
+        <div>
+            <button onClick={
+                () => setCount(count => count + 1)
+            }>
+                Increment Count ({count})
+            </button>
+            <h2>Time is {time}</h2>
+        </div>
+    )
 }
 
-export default UseEffectCounter;
+export default UseEffectCounter
